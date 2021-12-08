5f5740cfb9fa236090ce22b17921ad4d24e02800
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2021-12-08 08:32:46 -0600

    put in a timestamped dir

--- f5-diagnostics-jdk11/src/main/java/LogCSVExportUtility.java
+++ f5-diagnostics-jdk11/src/main/java/LogCSVExportUtility.java
@@ -5,13 +5,17 @@ import org.apache.commons.io.FileUtils;
 
 import java.io.File;
 import java.io.FileReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Iterator;
 
 public class LogCSVExportUtility {
     public static void main(String[] args) throws Exception {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy.MM.dd_HH.mm.ss.SSS");
         File csvFile = new File(args[0]);
         File outDir = new File(args[1]);
-        outDir.mkdirs();
+        File dumpDir = new File(outDir, df.format(LocalDateTime.now()));
+        dumpDir.mkdirs();
         final CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader().withSkipHeaderRecord(false);
         FileReader fr = new FileReader(csvFile);
         CSVParser parser = csvFormat.parse(fr);
@@ -25,7 +29,8 @@ public class LogCSVExportUtility {
             String message_txt = record.get(parser.getHeaderMap().get("message_txt"));
             String thread_s = record.get(parser.getHeaderMap().get("thread_s"));
             String caller_line_number_s = record.get(parser.getHeaderMap().get("caller_line_number_s"));
-            FileUtils.writeStringToFile(new File(outDir, kubernetes_pod_name_s + ".log"), String.format("%s - %s [%s:%s@%s] - %s%n", timestamp_tdt, level_s, thread_s, logger_class_s, caller_line_number_s, message_txt), true);
+            FileUtils.writeStringToFile(new File(dumpDir, kubernetes_pod_name_s + ".log"), String.format("%s - %s [%s:%s@%s] - %s%n", timestamp_tdt, level_s, thread_s, logger_class_s, caller_line_number_s, message_txt), true);
         }
+        System.out.println("Exported logs to " + dumpDir);
     }
 }
